1596b0916d195845b7aa5b1d5697894d45313dc2
Sarphu Rongpi <tomonso.ejang@gmail.com>
2021-09-02 09:49:30 +0530

    renamed index.js to app.js

--- app.js
+++ app.js
@@ -14,16 +14,17 @@ require("dotenv").config();
 //importing from local files
 const businessRoute = require("./src/routes/business");
 const evaluationRoute = require("./src/routes/evaluation");
+const roleRoute = require("./src/routes/role")
 
 
 
 //connecting to mongodb database
 
-
-mongoose.connect("mongodb://localhost:27017/raisingstars", {
+let mongoDBUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/raisingstars"
+mongoose.connect(mongoDBUrl, {
     useNewUrlParser: true,
    
-    
+
 })
     .then(() => {
         console.log('DB CONNECTED!!')
@@ -39,22 +40,20 @@ mongoose.connect("mongodb://localhost:27017/raisingstars", {
 
 
 //initialaising port no
-const port = process.env.SERVER_PORT || 8000;
+const port = process.env.SERVER_PORT || 3000;
 
 
-
-
+app.use(express.json())
 
 //  Routes
-
-app.get("/", (req, res) =>
-    console.log("hello  server is working!!!")
-
-)
+app.get("/", (req, res) => {
+    return res.send({message: "hello world"})
+})
 
 // my routes
 app.use("/api", businessRoute);
 app.use("/api", evaluationRoute);
+app.use("/api", roleRoute);
 
 //app.use("/api", authRoute);
 
