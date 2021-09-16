08b9bae8c2015cc284606c7d76364a6ed0911228
unknown <sudarshankrishna549@gmail.com>
2021-09-16 10:50:43 +0530

    member validation added ready to marge with development

--- src/routes/Member.js
+++ src/routes/Member.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const {
-    createMemberValidationRules, 
+    createMemberValidationRules, updateMemberValidationRules, 
   } = require("../validations/member");
 
 const member = require("../controllers/Member");
@@ -11,9 +11,9 @@ const  validate  = require("../validations/validate");
 
 
 
-router.get("/member", member.getAllMember);
+router.get("/member",createMemberValidationRules(), member.getAllMember);
 router.post("/member", member.create);
-router.put("/member/:id",createMemberValidationRules(),validate, member.update);
+router.put("/member/:id",updateMemberValidationRules(),validate, member.update);
 router.get("/member/:id", member.getEmergencyContact);
 router.delete("/member/:id", member.delete);
 router.put("/member/:memberId/:businessId",member.addMembership);
