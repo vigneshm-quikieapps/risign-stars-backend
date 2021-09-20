e57215c7b22700a5c28626c0b545819ed5009913
unknown <sudarshankrishna549@gmail.com>
2021-09-20 15:51:57 +0530

    validation folder change

--- src/validations/member.js
+++ src/validations/member.js
@@ -6,7 +6,7 @@ const createMemberValidationRules = () => {
     return [
       body("userId", "min length should be 2").isLength({ min: 2 }),
       body("membership").isArray(),
-      body("membership.*.business", "min length should be 2").custom(businessIdValidation),
+      body("membership.*.businessId", "min length should be 2").custom(businessIdValidation),
       body("membership.*.clubMembershipId", "min length should be 2").isLength({ min: 2 }),
       body("firstName", "min length should be 2 and max length should be 70").isLength({ min: 2, max:70 }),
       body("lastName", "min length should be 2 and max length should be 70").isLength({ min: 2, max:70}),
@@ -27,7 +27,7 @@ const createMemberValidationRules = () => {
     return [
       body("userId", "min length should be 2").optional().isLength({ min: 2 }),
       body("membership").optional().isArray(),
-      body("membership.*.business", "min length should be 2").optional().custom(businessIdValidation),
+      body("membership.*.businessId", "min length should be 2").optional().custom(businessIdValidation),
       body("membership.*.clubMembershipId", "min length should be 2").optional().isLength({ min: 2 }),
       body("firstName", "min length should be 2 and max length should be 70").optional().isLength({ min: 2, max:70 }),
       body("lastName", "min length should be 2 and max length should be 70").optional().isLength({ min: 2, max:70}),
