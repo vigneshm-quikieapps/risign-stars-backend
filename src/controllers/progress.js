c095000e1078e33d5896c872f5045442fd630d14
prahalad <prahaladhazarika1@gmail.com>
2021-10-04 12:20:27 +0530

    updated progress record

--- src/controllers/progress.js
+++ src/controllers/progress.js
@@ -1,4 +1,5 @@
 const Progress = require("../models/progress");
+const { EvaluationScheme, BusinessClass } = require("../models");
 
 const { validationResult } = require("express-validator");
 
@@ -15,62 +16,42 @@ module.exports.getProgressIdById = (req, res, next, id) => {
   });
 };
 
-//pogress creation
+/**
+ *
+ * function for progress payload data
+ */
 
-module.exports.createProgress = async (data, session) => {
-  // const errors = validationResult(req);
-
-  // if (!errors.isEmpty()) {
-  //     return res.status(422).json({
-  //         error: errors.array()[0].msg,
-  //     });
-  // }
-  const progress = new Progress(data);
-  await progress.save().session(session);
-  // progress.save((err, progress) => {
-  //     if (err) {
-  //         console.log(err);
-  //         console.log(req.body);
-
-  //         return res.status(400).json({
-  //             error: "unable to save progress to database",
-  //         });
-  //     }
-  //     res.json(progress);
-  // });
+const processSkill = (skill) => {
+  return { name: skill, status: "NOT_STARTED" };
 };
 
-//pogress listing all
-
-module.exports.getAllProgress = async (req, res) => {
-  try {
-    const activity = await Progress.find({});
-
-    if (!activity) {
-      return res
-        .status(200)
-        .json({ success: true, message: "No activity added." });
-    } else {
-      res.status(200).json({
-        success: true,
-        message: "Showing activity...",
-        activity: activity,
-      });
-    }
-  } catch (err) {
-    res.status(400).json({ success: false, message: err.message });
-  }
+const processLevel = (level) => {
+  return { skills: level.skills.map(processSkill), status: "NOT_STARTED" };
 };
 
-//pogress listing
-
-module.exports.getProgress = (req, res) => {
-  return res.json(req.progress);
+const genLevelsData = (evaluation) => {
+  return evaluation.levels.map(processLevel);
 };
 
-//pogress Update
+const progressPayloadRequest = async (req) => {
+  // console.log(req);
+  let { memberId, businessId, evaluationId } = req;
+  // let { memberId, businessId } = progressData;
+  // let businessClass = await BusinessClass.findById(businessId);
+  let evaluation = await EvaluationScheme.findById(evaluationId);
+  let { levelCount } = evaluation;
+  let levels = genLevelsData(evaluation);
+  return {
+    memberId,
+    businessId,
+    levelCount,
+    levels,
+  };
+};
 
-module.exports.updateProgress = (req, res) => {
+//progress creation
+
+module.exports.createProgress = async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -78,48 +59,218 @@ module.exports.updateProgress = (req, res) => {
       error: errors.array()[0].msg,
     });
   }
+  let progressPayload = await progressPayloadRequest(req.body);
+  const progress = new Progress(progressPayload);
+  progress.save((err, progress) => {
+    if (err) {
+      console.log(err);
+      console.log(req.body);
 
-  Progress.findByIdAndUpdate(
-    { _id: req.progress._id },
-    { $set: req.body }, //$push
+      return res.status(400).json({
+        error: "unable to save progress to database",
+      });
+    }
+    res.json(progress);
+  });
+};
 
-    { new: true, useFindAndModify: false },
-    (err, progress) => {
-      if (err) {
-        return res.status(400).json({
-          err: "you are not ",
-        });
-      }
+//get a progress record of a member
 
-      res.json(progress);
+module.exports.getAProgress = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  try {
+    const progress = await Progress.findone({
+      memberId: req.body.memberId,
+      businessId: req.body.businessId,
+    });
+
+    if (!progress) {
+      throw new Error("No Progress found");
+    }
+    res.status(200).json({
+      success: true,
+      message: "Showing progress record of a member",
+      progress: progress,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+/**
+ *
+ * marking a progress functionality
+ */
+
+const markingProgress = async (req, status) => {
+  const markedProgressData = await Progress.updateOne(
+    {
+      _id: req.body.progressId,
+      "levels.skills._id": req.body.skillId,
+    },
+    {
+      $set: {
+        "levels.$.skills.$[skill].status": status,
+      },
+    },
+    {
+      arrayFilters: [
+        // { "level.name": req.params.model },
+        { "skill._id": req.body.skillId },
+      ],
+    }
+  );
+
+  return markedProgressData;
+};
+
+/**
+ *
+ * marking progress date functionality
+ */
+
+const markingProgressDate = async (req, date) => {
+  await Progress.updateOne(
+    {
+      _id: req.body.progressId,
+      "levels.skills._id": req.body.skillId,
+    },
+    {
+      $set: {
+        "levels.$.skills.$[skill].startedAt": date,
+      },
+    },
+    {
+      arrayFilters: [
+        // { "level.name": req.params.model },
+        { "skill._id": req.body.skillId },
+      ],
     }
   );
 };
 
-//push new attendence ink progress
+/**
+ *
+ * marking attained date functionality
+ */
 
-module.exports.addAttendence = (req, res) => {
-  // const errors = validationResult(req);
+const markingAttainedDate = async (req, date) => {
+  await Progress.updateOne(
+    {
+      _id: req.body.progressId,
 
-  // if (!errors.isEmpty()) {
-  //     return res.status(422).json({
-  //         error: errors.array()[0].msg,
-  //     });
-  // }
-
-  Progress.findByIdAndUpdate(
-    { _id: req.progress._id },
-    { $push: { attend: req.body } },
-
-    { new: true },
-    (err, progress) => {
-      if (err) {
-        return res.status(400).json({
-          err: "you are not able to add attend ",
-        });
-      }
-
-      res.json(progress);
+      "levels.skills._id": req.body.skillId,
+    },
+    {
+      $set: {
+        "levels.$.skills.$[skill].completedAt": date,
+      },
+    },
+    {
+      arrayFilters: [{ "skill._id": req.body.skillId }],
     }
   );
 };
+
+/**
+ *
+ * mark a skill as progress
+ */
+
+module.exports.markAProgress = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  try {
+    let message = "";
+    if (req.body.status === "IN_PROGRESS") {
+      let date = new Date();
+      await markingProgressDate(req, date);
+      message = await markingProgress(req, "IN_PROGRESS");
+    } else {
+      let date = new Date();
+      await markingAttainedDate(req, date);
+      message = await markingProgress(req, "AWARDED");
+    }
+
+    if (!message) {
+      throw new Error("No progress record");
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Skill updation successfull",
+      progress: message,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+};
+
+/** code deprecated below this line */
+
+// //pogress listing all
+
+// module.exports.getAllProgress = async (req, res) => {
+//   try {
+//     const activity = await Progress.find({});
+
+//     if (!activity) {
+//       return res
+//         .status(200)
+//         .json({ success: true, message: "No activity added." });
+//     } else {
+//       res.status(200).json({
+//         success: true,
+//         message: "Showing activity...",
+//         activity: activity,
+//       });
+//     }
+//   } catch (err) {
+//     res.status(400).json({ success: false, message: err.message });
+//   }
+// };
+
+// //pogress listing
+
+// module.exports.getProgress = (req, res) => {
+//   return res.json(req.progress);
+// };
+
+// //pogress Update
+
+// module.exports.updateProgress = (req, res) => {
+//   const errors = validationResult(req);
+
+//   if (!errors.isEmpty()) {
+//     return res.status(422).json({
+//       error: errors.array()[0].msg,
+//     });
+//   }
+
+//   Progress.findByIdAndUpdate(
+//     { _id: req.progress._id },
+//     { $set: req.body }, //$push
+
+//     { new: true, useFindAndModify: false },
+//     (err, progress) => {
+//       if (err) {
+//         return res.status(400).json({
+//           err: "you are not ",
+//         });
+//       }
+
+//       res.json(progress);
+//     }
+//   );
+// };
