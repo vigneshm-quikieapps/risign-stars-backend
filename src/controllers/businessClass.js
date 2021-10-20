50f3809363edca734e683a2df958f3caa4304941
unknown <sudarshankrishna549@gmail.com>
2021-10-20 16:12:14 +0530

    session id populate added

--- src/controllers/businessClass.js
+++ src/controllers/businessClass.js
@@ -4,7 +4,7 @@ const { getQuery, getOptions } = require("../helpers/query");
 //parameter extractor
 module.exports.getBusinessClassIdById = (req, res, next, id) => {
   BusinessClass.findById(id)
-    .populate("sessionIds")
+    .populate("businessId", "sessionIds")
     .exec((err, Class) => {
       if (err) {
         return res.status(400).json({
