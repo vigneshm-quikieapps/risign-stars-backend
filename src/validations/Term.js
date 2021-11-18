802a8b63a967fd6f7d610f8f3d268119e6f647b2
aditya vinayak <aditya@quikieapps.com>
2021-11-18 17:25:34 +0530

    create term validation rules and termFee in Modal

--- src/validations/Term.js
+++ src/validations/Term.js
@@ -17,6 +17,10 @@ const createTermValidationRules = () => {
       format: "YYYY-MM-DD",
       strictMode: true,
     }),
+    check(
+      "termFee",
+      "term fee should be a positive Number"
+    ).isInt({min:0}),
     // check(
     //   "classsequence",
     //   "classsequence should be a Number/Integer  "
@@ -38,6 +42,11 @@ const updateTermValidationRules = () => {
       "startdate",
       "endtime   should be a date in format: 'MM-DD-YYYY'"
     ).optional(),
+    check(
+      "termFee",
+      "term fee should be a positive Number"
+    ).optional()
+    .isInt({min:0}),
     // check("classsequence", "classsequence should be a Numbre/Integer  ")
     //   .optional()
     //   .isInt(),
