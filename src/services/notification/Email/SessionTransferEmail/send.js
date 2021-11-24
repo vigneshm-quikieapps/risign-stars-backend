95561b1d04d0646d31954cde9e6acf20d8ddbb9b
aditya vinayak <aditya@quikieapps.com>
2021-11-24 15:38:11 +0530

    changedparameters of session transfer email functionality

--- src/services/notification/Email/SessionTransferEmail/send.js
+++ src/services/notification/Email/SessionTransferEmail/send.js
@@ -2,13 +2,13 @@ const sendEmail = require("../sendEmail");
 const { FROM } = require("../../../../constants/email");
 
 
-const send = ({to},{userData,oldSessionData,classData,newSessionData}) => {
+const send = ({to},{userData,businessSessionData,businessClassData,newSession}) => {
   const msg = {
     to,
     from: FROM, // Change to your verified sender
     subject: "Enrolled Sucessfull",
     text: "and easy to do anywhere, even with Node.js",
-    html: `<strong>${userData.name} has been successfully session transfer from ${oldSessionData.name} to ${newSessionData.name} of ${classData.name}</strong>`,
+    html: `<strong>${userData.name} has been successfully session transfer from ${businessSessionData.name} to ${newSession.name} of ${businessClassData.name}</strong>`,
   };
   sendEmail(msg);
 };
