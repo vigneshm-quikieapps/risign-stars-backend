b6b35f0fcadb6c9753cd782beab318eb7816a708
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-17 20:31:37 +0530

    set cors credentials to true, set origin to localhost:3000 and localhost:5000 instead of * so withCredentials can be used in the front-end development env

--- src/app/app.js
+++ src/app/app.js
@@ -5,13 +5,13 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 var corsOptions = {
-  credentials: process.env.NODE_ENV === "production",
+  credentials: true,
   origin:
     process.env.NODE_ENV === "production"
       ? process.env.ORIGINS.split(" ")
-      : "*",
+      : ["http://localhost:3000", "http://localhost:5000"],
 };
-
+console.log(corsOptions);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
