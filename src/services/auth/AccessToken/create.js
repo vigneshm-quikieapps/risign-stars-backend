fa80373324d4317e9f4a21b03b9c1daf950142d1
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-22 15:06:21 +0530

    change accessToken expiry to 7d as mobile-app team asked

--- src/services/auth/AccessToken/create.js
+++ src/services/auth/AccessToken/create.js
@@ -2,7 +2,8 @@ const { sign } = require("jsonwebtoken");
 
 const create = (payload) => {
   return sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.NODE_ENV === "production" ? 5 * 60 : "7d",
+    // expiresIn: process.env.NODE_ENV === "production" ? 5 * 60 : "7d",
+    expiresIn: "7d",
   });
 };
 
